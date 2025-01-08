ad96953dff8d9d62f25af71a0f6fa48aeb9dad70
Nagabandaru2003 <nagasarojabandaru@gmail.com>
2025-01-08 11:11:14 +0530

    done code

---
+++ Basics.py
@@ -0,0 +1,18 @@
+#Scripting way for waiting the code :
+print('Hello World')
+
+#Procedural way of Writing the code 
+
+def message():
+    print('Procedural way of Writing : Hello World ')
+message()
+
+
+#object oriented way of  writing the code :
+
+class Demo:
+    def instance_method(self):
+        print('Procedural way of Writing : Hello World')
+
+d1 = Demo()
+d1.instance_method()
---
+++ variablesDtatypes/Overview.py
@@ -0,0 +1,4 @@
+# pytho is dynamically typed programing language 
+# 2 In python while delcaring variables , we no need tp mention data types
+#3. Data Types will be assigned by language based on
+#VALUE STRORED INTO THE VARIABLES  
