862c30eb0f18e7eeaf541e551df1f3463d8918c7
Nagabandaru2003 <nagasarojabandaru@gmail.com>
2025-01-09 18:12:53 +0530

    done

---
+++ python start/functions.py
@@ -0,0 +1,22 @@
+
+def add():
+    a = 10
+    b = 5
+    print("Add nums:",a+b)
+
+
+def sub(a,b):
+    print("sub: ",a-b)
+    
+def mul():
+    return 10*20
+
+
+def div(a,b):
+    return a/b
+
+
+add()
+sub(100,50)
+print(mul())
+print(div(200,10))
