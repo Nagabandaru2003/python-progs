5bcd3ecb7bcd46e295c8137b60f1a048e2d6e55a
Nagabandaru2003 <nagasarojabandaru@gmail.com>
2025-01-09 17:31:00 +0530

    done

--- variablesDtatypes/typecasting.py
+++ variablesDtatypes/typecasting.py
@@ -1,10 +1,10 @@
 
-a = "30"
-b = int(a)
+# a = "30"
+# b = int(a)
 
 
-print(a, type(a))
-print(b, type(b))
+# print(a, type(a))
+# print(b, type(b))
 
 
 # x = "Kod"
@@ -14,5 +14,23 @@ print(b, type(b))
 # print(y, type)
 
 
-p = int(input("Enter int type data:"))
-print(p, type(p))
+# p = int(input("Enter int type data:"))
+# print(p, type(p))
+
+
+
+# bool 
+
+naga = input("I am an developer (yes/no): ").lower() 
+dev = naga == "yes" 
+print("Naga is a developer:", dev)
+
+
+print(bool("Naga Bandaru")) #true
+print(int("naga")) #error
+print(int("11")) #11
+print(float("22.22"))
+print(bool(" "))
+print(bool(0))
+print(int(12))
+# print(int("12.5")) # not allowed 
