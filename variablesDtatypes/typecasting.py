24ee9ced52b919397b9d7ce7a1413317fa96b72b
Nagabandaru2003 <nagasarojabandaru@gmail.com>
2025-01-09 17:00:42 +0530

    done

---
+++ variablesDtatypes/typecasting.py
@@ -0,0 +1,18 @@
+
+a = "30"
+b = int(a)
+
+
+print(a, type(a))
+print(b, type(b))
+
+
+# x = "Kod"
+
+ # print(x, type(x))
+# y = int(x)
+# print(y, type)
+
+
+p = int(input("Enter int type data:"))
+print(p, type(p))
