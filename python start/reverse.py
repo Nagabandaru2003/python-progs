35dfc792f542c1273ea3a5bb6177f0485a05184d
Nagabandaru2003 <nagasarojabandaru@gmail.com>
2025-01-17 16:51:09 +0530

    done

--- python start/reverse.py
+++ python start/reverse.py
@@ -1,4 +1,13 @@
+
+#reverse() will reverse the original object
+
 li = [10,5,20,7,40]
 print("before reverse: ",li)
 li.reverse()
 print("After reverse: ",li)
+
+#reversed(iterable_object): 
+li2 = [11,6,8,22]
+reversed_li2=list(reversed(li2))
+print(li2) 
+print(reversed_li2)
