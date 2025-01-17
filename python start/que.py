9f2867bbc9b72edd0fdb7289afd8ae0e61931822
Nagabandaru2003 <nagasarojabandaru@gmail.com>
2025-01-17 18:34:11 +0530

    done

--- python start/que.py
+++ python start/que.py
@@ -1,1 +1,9 @@
-li = list(map(input().split()))
+li = list(map(int,input().split()))
+d = {10:2,20:1,30:1}
+# num,count= [10,2]
+
+for num, count in d.items():
+    print(f"{num} occures {count} tim10e(s)")
+    
+
+
